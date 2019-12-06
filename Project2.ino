dda8c15ef685b9d621ce784356322cdcd195c7ea
IgaGonzalez <34428045+IgaGonzalez@users.noreply.github.com>
2019-12-05 20:56:24 -0600

    Add files via upload
    Added comments to code.
    Cleaned up the code.

--- Project2.ino
+++ Project2.ino
@@ -2,132 +2,75 @@
 #include <PulseSensorPlayground.h>
 
 const long s = 10; //size of Array
-long buf[s];
-long f=0, b=0;
-long aux = 0;
-long aux1=0;
+long buf[s];      //Declare the array
+long f=0, b=0;   // Front and back indexes for order
+long aux = 0;    // Keep the averages in this variable
+long aux1=0;     // Auxiliar variable used in the debugging
 
-long avg[s];
-long avg1;
-long acum = 0;
+long avg[s];  // Declare array of averages, same size the input array
+long avg1;    // Variable used to express the input
+long acum = 0; 
 const int PulseSensorPurplePin = 5;
 int Threshold = 650; 
 PulseSensorPlayground pulseSensor;
 
-void average_Averages(){
-  for(int i = 0; i < s; i++){
-    acum += avg[i];
-    if(i == 9){
-      i == 0;
-      acum = acum / s;
-      Serial.print("Average of 10 last averages");
-      Serial.println(acum);
-      acum = 0;
-    } 
-  }
-}
-
-void enqueue(long num){
-  long nf = (f+1)%s;
-  if(nf != b){
+void enqueue(long num){ // First function used to control the array. We start to save the input data in the array with this function. Recieves a long datatype as parameter
+  long nf = (f+1)%s;  // Makes nf the index in front of f, if f is in the final index of the buffer, goes to the first value
+  if(nf != b){ //Checks that nf does not takes position of the back index
     f = nf;
     buf[f] = num;
     aux += num;
+    //The previous lines changes the index to keep the same order. First data in, Last out
     
     //Serial.print("Constant Average: ");
-    //Serial.println(aux/s);
-    avg1 = map(aux/s,0,200,200,1000);
-    Serial.println(avg1);
+    //Serial.println(aux/s); //aux/s is the constant average of the array.
+
+    //The regular EDA data input fluctuates between 0 and 200 (give or take). 
+    avg1 = map(aux/s,0,200,200,1000); //We amplify the data in order to make a more understandable plot of this data. 
+    Serial.println(avg1); 
     Serial.println("\t");
     
-    /*
-     * for(int i = 0; i < s; i++){
-      avg[i] = aux/s;
-      if (i==9){
-        i == 0;
-        average_Averages();
-      }
-    }
-    */
-    aux1 += 1; 
+    aux1 += 1; //For debugging purposes
   } else {
     //Serial.println("Full");
-    long val = dequeue();
+    long val = dequeue(); // If the nf index is the same as the back index, then dequeue the last value in the buffer.
     //Serial.print("Dequeue: ");
     //Serial.println(val);
     
-    Serial.print("Average: ");
-    Serial.println(aux/aux1);
+    //Serial.print("Average: ");
+    //Serial.println(aux/aux1);
   }
 }
 
-// long avg(Array buf, )
-
-int dequeue(){
-  if(b != f){
-    long retval = buf[b];
-    buf[b] = 0;
-    b = (b+1)%s;
-    aux -= retval;
+int dequeue(){ 
+  if(b != f){              //Checks if the back index is not the same as the front index. 
+    long retval = buf[b];  //Save the last value
+    buf[b] = 0;             // Turns the value to 0
+    b = (b+1)%s;            
+    aux -= retval;          //Substract the value from the total average
     aux1 -= 1;
-    return retval;
+    return retval;        //Returns the value discarded
   }
   else {
     //Serial.println("EMPTY");
   }
   return -1;
 }
-const int OUTPUT_TYPE = SERIAL_PLOTTER;
-const int PULSE_FADE = 5;
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  /*
-  pulseSensor.analogInput(PulseSensorPurplePin);
-  pulseSensor.blinkOnPulse(13);
-  pulseSensor.fadeOnPulse(PULSE_FADE);
-  
-  pulseSensor.setSerial(Serial);
-  pulseSensor.setOutputType(OUTPUT_TYPE);
-  pulseSensor.setThreshold(Threshold);
-
-   if (!pulseSensor.begin()) {
-    /*
-       PulseSensor initialization failed,
-       likely because our particular Arduino platform interrupts
-       aren't supported yet.
-
-       If your Sketch hangs here, try PulseSensor_BPM_Alternative.ino,
-       which doesn't use interrupts.
-    
-    for(;;) {
-      // Flash the led to show things didn't work.
-      digitalWrite(13, LOW);
-      delay(50);
-      digitalWrite(13, HIGH);
-      delay(50);
-    }
-    
-  }*/
+  Serial.begin(115200); // It must be in this value for the arduino to better handle the I/O of data. 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   delay(20);
-  /*
-  int Signal = analogRead(PulseSensorPurplePin);
-  Serial.print(Signal);
+  
+  int Signal = analogRead(PulseSensorPurplePin); //Get the reading from the heart rate sensor and save in Signal
+  Serial.print(Signal); 
   Serial.print("\t");
-  */
-  /*
-  if(random(2) > 0){
-    enqueue(analogRead(A0));
-  } else {
-    long val = dequeue();
-    Serial.print("Dequeue: ");
-    Serial.println(val);
-  }*/
   enqueue(analogRead(A0));
+  //The code below is left in order for better understanding how the buffer works. 
   /*
   for(int i = 0; i < s; i++){
     Serial.print(buf[i]);
@@ -144,10 +87,4 @@ void loop() {
     Serial.print("\t");
   }
   Serial.println(""); */
-  //Serial.println(analogRead(sensorPin));
-  
-  //delay(100);
-  //Serial.println(analogRead(A0));
-
-  
 }
